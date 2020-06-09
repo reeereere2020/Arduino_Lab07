54a9193a4240076f8e0a2ea5e148c440d9a6dab1
reeereere2020 <61824449+reeereere2020@users.noreply.github.com>
2020-06-09 20:17:07 +0800

    Add files via upload

---
+++ Lab07_Part1.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+
+void setup() {
+  lcd.begin(20, 2); 
+  lcd.print("05052421");
+  lcd.print("yty");.
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
+}
+
